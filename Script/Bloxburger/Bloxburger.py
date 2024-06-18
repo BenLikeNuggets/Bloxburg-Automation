9e10a400221114dc9d764d8716007c09ac508d7e
BenLikeNuggets <benersej927@gmail.com>
2024-06-18 15:33:36 +0200

    Continuing work on 'Bloxburger.py'. Setup section is complete. Trying to make the script automatically take the burger orders after setup. Currently errors in locating the orders, but trying to fix it.

--- Script/Bloxburger/Bloxburger.py
+++ Script/Bloxburger/Bloxburger.py
@@ -7,6 +7,7 @@ from pynput.mouse import Button, Controller
 mouse_controller = Controller()
 
 # ___________________Storing the variables by image names___________________
+# ______Declaring the variables of the menu______
 # Menu region:
 Menu_region = "Menu_region.png"
 
@@ -38,6 +39,31 @@ Medium = "Medium.png"
 Large = "Large.png"
 Confirm_button = "Confirm.png"
 
+# ______Declaring the variables of the customers' orders______
+# Customers' orders region:
+Customer_region = "Customer_region.png"
+
+# Orders - Burger
+Cheese_1 = "Cheese_1.png"
+Cheese_2 = "Cheese_2.png"
+Lettuce_1 = "Lettuce_1.png"
+Lettuce_2 = "Lettuce_2.png"
+Normal_beef_1 = "Normal_beef_1.png"
+Normal_beef_2 = "Normal_beef_2.png"
+Onion_1 = "Onion_1.png"
+Onion_2 = "Onion_2.png"
+Tomato_1 = "Tomato_1.png"
+Tomato_2 = "Tomato_2.png"
+Vegan_beef_1 = "Vegan_beef_1.png"
+Vegan_beef_2 = "Vegan_beef_2.png"
+
+# Orders - Fries
+
+
+# Orders - Drinks
+
+
+
 # ___________________Functions used in the following program___________________
 # Function for locating the center of the images. Searches in a given region.
 def locate_image(image, region=None):
@@ -62,14 +88,57 @@ def locate_and_click(image, region=None):
             pyautogui.press('8')
             time.sleep(0.2)
             print(f"{image} is located at: {image_location}")
+            return image_location
         else:
             print(f"Image {image} not found on screen.")
     except pyautogui.ImageNotFoundException:
         print(f"Image {image} not found (exception caught).")
 
-# Define which key the program is listening to
+def order_click_1(order, menu):
+    try:
+        order_location = pyautogui.locateOnScreen(order, confidence=0.95, region=Customer_region)
+        if order_location:
+            pyautogui.moveTo(menu)
+            pyautogui.press('8')
+            time.sleep(0.2)
+        else:
+            None
+    except pyautogui.ImageNotFoundException:
+        print(f"Image {menu} not found (exception caught).")
+
+def order_click_2(order, menu):
+    try:
+        order_location = pyautogui.locateOnScreen(order, confidence=0.95, region=Customer_region)
+        if order_location:
+            pyautogui.moveTo(menu)
+            pyautogui.press('8')
+            time.sleep(0.2)
+            pyautogui.leftClick()
+            time.sleep(1.0)
+        else:
+            print(f"Image {menu} not found on screen.")
+    except pyautogui.ImageNotFoundException:
+        print(f"Image {menu} not found (exception caught).")
+
+
+# The script will act differently depending on which key you press.
 def on_press(key):
     try:
+        if key.char == 'g': 
+            # ______Setup for location of the customer region______
+            # Reference the global variables from the start:
+            global Customer_region
+
+            # Locating the image 'Customer_region' on screen. 
+            Customer_region = pyautogui.locateOnScreen(Customer_region, confidence=0.8)
+            if Customer_region:
+                left, top, width, height = Customer_region
+                # Assigning the location to the variable: 'Customer_region'
+                Customer_region = (left, top, width, height)
+                print(f"Copy & paste this to the next section:\n{Customer_region}")
+            else:
+                print(f"The region wasn't found")
+
         if key.char == 'f': 
             # ______Setup for location of the menu region______
             # Reference the global variables from the start:
@@ -81,6 +150,7 @@ def on_press(key):
                 left, top, width, height = Menu_region
                 # Assigning the location to the variable: 'Menu_region'
                 Menu_region = (left, top, width, height)
+                print(f"Location of the menu_region is: {Menu_region}")
             else:
                 print(f"The region wasn't found")
 
@@ -136,6 +206,36 @@ def on_press(key):
 
             locate_and_click(Confirm_button, region=Menu_region)
 
+            time.sleep(2.75)
+
+            # ______Assigned customer region for this section of the script______
+            Customer_region = (670, 369, 603, 97)
+
+            # ______Clicking customers burger orders______
+            order_click_1(Cheese_1, Cheese)
+            order_click_2(Cheese_2, Cheese)
+            order_click_1(Lettuce_1, Lettuce)
+            order_click_1(Lettuce_2, Lettuce)
+            order_click_1(Normal_beef_1, Normal_beef)
+            order_click_2(Normal_beef_2, Normal_beef)
+            order_click_1(Onion_1, Onion)
+            order_click_2(Onion_2, Onion)
+            order_click_1(Tomato_1, Tomato)
+            order_click_2(Tomato_2, Tomato)
+            order_click_1(Vegan_beef_1, Vegan_beef)
+            order_click_2(Vegan_beef_2, Vegan_beef)
+
+
+            # ______Clicking customers fry orders______
+
+
+            # ______Clicking customers drink orders______
+
+
+            # ______Clicking confirmation button______
+
+        if key.char == 'h':
+            f=1
 
 
 
